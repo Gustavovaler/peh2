8bfd2dd28fc30d1c7a3596880666e042ed975110
gustavo <gustavodevaler@gmail.com>
2019-03-24 23:03:32 -0300

    item

--- item.php
+++ item.php
@@ -1,5 +1,5 @@
 <?php
-require("global/config.php");
+//require("global/config.php");
 require("templates/header.php");
 require("global/conexion2.php");
 
@@ -11,6 +11,14 @@ $row=mysqli_fetch_array($resultado);
 
 
 ?>
+<link rel="stylesheet" type="text/css" href="css/item.css">
+<form action="categoria.php" method="POST">
+	<input type="submit" name="boton" value="Volver al listado">
+	<input type="hidden" name="categoria" value="<?php echo $row['categoria'] ;?>">
+</form>
+<!-- ************************************************************ -->
+<div class="container">
+
 <link rel="stylesheet" type="text/css" href="css/item.css">
 <div class="container">
 	<div class="image">
@@ -26,4 +34,4 @@ echo "<table >
 <tr><td ><img src='".$row['foto']."' width='70'><td>".$row["descripcion"]."</td><td>".$row['talle']."</td><td>".$row['categoria']."</td><td>".$row['articulo']."</td><td></td><td>".$row['precio']."</td><td></tr></table>";
 ?>
 
-
+</div>
