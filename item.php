56e38a351b37ca601b4033799693a17b829b6c7c
gustavo <gustavodevaler@gmail.com>
2019-03-25 03:20:09 -0300

    .

--- item.php
+++ item.php
@@ -24,17 +24,27 @@ $row=$producto;
 <link rel="stylesheet" type="text/css" href="css/item.css">
 <div class="container">
 	<div class="image">
-		 <img src='<?php echo $row['foto'] ;?>'>
+		  <img src="<?php echo $producto['foto'] ;?>" class="img-fluid" alt="Responsive image">
+
+
+
+
+
+
+
+ 
 		
 	</div>
 	
 </div>
 
-<?php
 
-echo "<table >
-<tr><td ><img src='".$row['foto']."' width='70'><td>".$row["descripcion"]."</td><td>".$row['talle']."</td><td>".$row['categoria']."</td><td>".$row['articulo']."</td><td></td><td>".$row['precio']."</td><td></tr></table>";
-?>
+
+
+
+
+
+
 <form action="" method="POST">
 
 					<input type="hidden" name="ID" value="<?php echo $producto['ID'];?>" >
